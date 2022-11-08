2623b36596796e83e1ed465ae30fa374d6b495cd
Ankam Suresh <ankamsuresh21@gmail.com>
2022-11-08 21:05:28 +0530

    added collection list

---
+++ src/com/prepinsta/collections/ListElement.java
@@ -0,0 +1,15 @@
+package com.prepinsta.collections;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class ListElement {
+	public static void main(String[] args) {
+		List<String> l = Arrays.asList("suresh", "bhanu", "kalyan");
+		for (String s : l) {
+			System.out.println(s);
+		}
+
+	}
+
+}
