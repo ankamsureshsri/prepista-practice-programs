47b67b706128f9d31320d435afb11558073c41ae
Ankam Suresh <ankamsuresh21@gmail.com>
2022-11-08 20:46:56 +0530

    Added swap two numbers

---
+++ src/com/prepinsta/numbers/SwapTwoNumbers.java
@@ -0,0 +1,11 @@
+package com.prepinsta.numbers;
+
+public class SwapTwoNumbers {
+	public static void main(String[] args) {
+		int a = 10, b = 20, temp = 0;
+		temp = a;
+		a = b;
+		b = temp;
+		System.out.println(a + " " + b);
+	}
+}
